a9f2e94aa0be2851509b5937b7d0b5ac48126865
RamitShapkota <shapkotaramit47@gmail.com>
2026-08-14 21:39:59 +0545

    refactor: remove HMR configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -34,10 +34,6 @@ export default defineConfig(({ mode }) => {
       port: parseInt(process.env.PORT || '8443'),
       strictPort: true,
       watch: { ignored: ['**/.figma/**'] },
-      hmr: {
-        clientPort: 443,
-        protocol: 'wss',
-      },
     },
     preview: {
       host: '0.0.0.0',
